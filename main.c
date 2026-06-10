97d853d2faa38718095013a9843704af5047dcd0
saanvi-s-gowda <saanvigsowda13@gmail.com>
2026-06-10 09:55:59 +0530

    Implement drawing menu with user choices

--- main.c
+++ main.c
@@ -110,3 +110,57 @@ printf("Canvas size:%dx%d\n";WIDTH,HEIGHT);
 printf("Use coordinates xy.\n");
 printf("x range:0 to %d\n",WIDTH-1);
 printf("y range:0 to %d\n",HEIGHT-1);
+while(1){
+printf("\nMenu\n");
+printf("1.Draw Line\n");
+printf("2.Draw Rectangle\n");
+printf("3.Draw Circle\n");
+printf("4.Draw Triangle\n");
+printf("5.Display Picture\n");
+printf("().Exit\n");
+printf("Enter Choice:");
+scanf("%d",&choice);
+if(choice==1){
+int x1,y1,x2,y2;
+printf("Enter x1 y1 x2 y2:");
+scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+drawLine(x1,y1,x2,y2);
+}
+else if(choice==2){
+int x1,y1,x2,y2;
+printf("Enter top-left xy and bottom-right xt:");
+scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+drawRectangle(x1,y1,x2,y2);
+}
+        else if (choice == 3) {
+            int cx, cy, radius;
+
+            printf("Enter center x y and radius: ");
+            scanf("%d %d %d", &cx, &cy, &radius);
+
+            drawCircle(cx, cy, radius);
+        }
+        else if (choice == 4) {
+            int x1, y1, x2, y2, x3, y3;
+
+            printf("Enter x1 y1 x2 y2 x3 y3: ");
+            scanf("%d %d %d %d %d %d",
+                  &x1, &y1, &x2, &y2, &x3, &y3);
+
+            drawTriangle(x1, y1, x2, y2, x3, y3);
+        }
+        else if (choice == 5) {
+            printf("The picture is:\n");
+            displayPicture();
+        }
+        else if (choice == 0) {
+            printf("Exiting program.\n");
+            break;
+        }
+        else {
+            printf("Invalid choice.\n");
+        }
+    }
+
+    return 0;
+}
